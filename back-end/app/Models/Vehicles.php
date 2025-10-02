ed8cbaf95389a640d7165a96b601b520134e475a
nhanpro <nhunguyenthanhnhu83@gmail.com>
2025-10-02 09:34:55 +0700

    vehicles

--- back-end/app/Models/Vehicles.php
+++ back-end/app/Models/Vehicles.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicles extends Model
 {
-    //
+    protected $table = 'vehicles';
+
+    protected $primaryKey = 'id';
+
+
+    protected $fillable =['name',
+                            'vehicle_type',
+                        ];
+
+    
+
 }
